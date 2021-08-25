fd1c5741b958f09875d0c1918aafb4446c21d232
HelenBassa <helenbassa@gmail.com>
2021-08-25 10:56:50 +0300

    refactor: delete react-api files

--- react-api/src/index.tsx
+++
@@ -1,7 +0,0 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import App from './components/app/app';
-
-import './styles.scss';
-
-ReactDOM.render(<App />, document.getElementById('app'));
